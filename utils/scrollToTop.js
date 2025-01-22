15a76be4ae10c4843fa35c8e0bf1ed69bf21c740
Behnam <behnam.sep@gmail.com>
2025-01-22 15:56:57 +0100

    add form message and scroll to top function

--- utils/scrollToTop.js
+++ utils/scrollToTop.js
@@ -1,6 +1,18 @@
 const scrollToTop = () => {
-  
-  };
-  
-  export default scrollToTop;
-  
+  const scrollToTopButton = document.getElementById("scrollTop");
+  const toggleButtonVisibility  =()=>{
+    window.scrollY > 300
+      ? (scrollToTopButton.style.display = "block")
+      : (scrollToTopButton.style.display = "none");}
+  if (!scrollToTopButton) return;
+  window.addEventListener("scroll", toggleButtonVisibility );
+
+  scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+};
+
+export default scrollToTop;
